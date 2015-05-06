404537974eeec8c9ff77e16df9fee57b5e4ffdf9
Alfred Bez <alfred.bez@googlemail.com>
2015-05-06 15:30:26 +0200

    some logic

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo 'Hello World';
+require 'vendor/autoload.php';
+
+use League\Csv\Reader;
+
+$csv = Reader::createFromPath('dummy.csv');
+$headers = $csv->fetchOne();
+$res = $csv->setLimit(25)->fetchAll();
+
+echo "<pre>\n";
+print_r([
+        'headers' => $headers,
+        'res' => $res,
+]);
+echo "\n</pre>";
